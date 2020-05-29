c84716ce25cdd2a4a7e09cf8d51025b9a2622a4f
alanbanks229 <alanbanks229@gmail.com>
2020-05-29 18:30:13 -0400

    fixed bug causing duplicate <div> tags to appear after user navigates to different selection on website

--- src/App.js
+++ src/App.js
@@ -28,11 +28,22 @@ function App(props) {
     if (document.body.offsetWidth >= 980 ){
       // debugger
         var initial_panel = document.getElementById('navPanel')
+        var initial_nav_panel_toggle = document.getElementById('navPanelToggle')
+        var initial_bg_div = document.getElementsByClassName('bg')[0]
+
+        //noticed that introducing jquery into this website causes duplicate html
+        // tags to be constructed this is a hacky attempt to git rid of some of the duplicates.
         if (initial_panel){
           initial_panel.parentNode.removeChild(initial_panel)
         }
         for(let index = 0; index < arr_of_scripts.length; index++){
           loadjs(arr_of_scripts[index])
+        }
+        if (initial_bg_div){
+          initial_bg_div.parentNode.removeChild(initial_bg_div)
+        }
+        if (initial_nav_panel_toggle){
+          initial_nav_panel_toggle.parentNode.removeChild(initial_nav_panel_toggle)
         }
       console.log("Rendering display for labtop/desktop/monitor usage")
     } else {
