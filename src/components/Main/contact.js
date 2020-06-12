73f0fa9ed752936eb6ed19b490e50f4479194527
alanbanks229 <alanbanks229@gmail.com>
2020-06-11 23:17:06 -0400

    fixed contact page social links, they display same links as that seen in the nav bar

--- src/components/Main/contact.js
+++ src/components/Main/contact.js
@@ -92,10 +92,10 @@ function Contact() {
             <section>
                 <h3>Social</h3>
                 <ul class="icons alt">
-                    <li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
-                    <li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
-                    <li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
-                    <li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/alanbanks229" className="icon brands fa-github"><span className="label">GitHub</span></a></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alan-banks/" className="icon brands fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://medium.com/@alanbanks229" className="icon brands fa-medium"><span className="label">Medium</span></a></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/alan_got_banks/" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
                 </ul>
             </section>
         </section>
